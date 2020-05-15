94e51237685cf0bbae0afa24ee3677476cd455d7
Kosodrzewinatru <sienkiew@live.com>
2020-05-15 23:09:01 +0200

    added fillSeats function

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,19 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    public Button[][] fillSeats(GridPane gridPane) {
+        Button[][] seats = new Button[gridPane.getRowCount()][gridPane.getColumnCount()];
+
+        for (int i = 0; i < gridPane.getRowCount(); i++) {
+            for (int j = 0; j < gridPane.getColumnCount(); j++) {
+                Button temp = new Button();
+                gridPane.add(temp, j, i);
+                seats[i][j] = temp;
+            }
+        }
+
+        return seats;
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -14,6 +27,9 @@ public class App extends Application {
         Parent root = loader.load();
         GridPane sectionFirst =  (GridPane) loader.getNamespace().get("sectionFirst");
         GridPane sectionSecond =  (GridPane) loader.getNamespace().get("sectionSecond");
+
+        fillSeats(sectionFirst);
+        fillSeats(sectionSecond);
 
         stage.setScene(new Scene(root, 1280, 720));
         stage.show();
