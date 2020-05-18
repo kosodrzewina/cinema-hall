6c54cb832297d52b3201b6c45c9557795f91913d
Kosodrzewinatru <sienkiew@live.com>
2020-05-18 18:56:54 +0200

    seats are enabled after logging in

--- src/main/java/HallController.java
+++ src/main/java/HallController.java
@@ -1,6 +1,6 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 
 public class HallController {
@@ -10,17 +10,23 @@ public class HallController {
     @FXML
     private PasswordField passwordField;
 
-    @FXML
-    private Label test;
-
     public static boolean getAccess() {
         return access;
+    }
+
+    private void unlockSeats(Button[][] seats) {
+        for (int i = 0; i < seats.length; i++)
+            for (int j = 0; j < seats[i].length; j++)
+                seats[i][j].setDisable(false);
     }
 
     public void onSubmitPassword(ActionEvent actionEvent) {
         if (passwordField.getText().equals(password)) {
             access = true;
-            test.setText("granted");
+            passwordField.setPromptText("zalogowano");
+            passwordField.setDisable(true);
+            unlockSeats(App.sectionFirstSeats);
+            unlockSeats(App.sectionSecondSeats);
         }
 
         passwordField.setText("");
