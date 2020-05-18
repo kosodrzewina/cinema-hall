00e6911b3e123e075558c7b2c0f63613ee074930
Kosodrzewinatru <sienkiew@live.com>
2020-05-18 13:59:54 +0200

    added password verification

---
+++ src/main/java/HallController.java
@@ -0,0 +1,28 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+
+public class HallController {
+    private String password = "haslo123";
+    private boolean access = false;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label test;
+
+    public boolean getAccess() {
+        return access;
+    }
+
+    public void onSubmitPassword(ActionEvent actionEvent) {
+        if (passwordField.getText().equals(password)) {
+            access = true;
+            test.setText("granted");
+        }
+
+        passwordField.setText("");
+    }
+}
